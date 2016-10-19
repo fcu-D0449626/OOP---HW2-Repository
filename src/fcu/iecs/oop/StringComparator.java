f7d445a2c9a3c25eba8214a3d7f6cb739d84ca20
fcu-d0449626 <su830323@gmail.com>
2016-10-19 17:00:58 +0900

    HW3

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -3,18 +3,26 @@ import java.util.Scanner;
 public class StringComparator {
 
 	public static void main(String[] args) {
+		boolean run=false;
 		Scanner scanner = new Scanner(System.in);
 		String a,b;
-		System.out.println("Enter a string 1");
-		a=scanner.next();
-		System.out.println("Enter a string 2");
-		b=scanner.next();
+		
+		do{
+			System.out.println("Enter a string 1");
+			a=scanner.next();
+			System.out.println("Enter a string 2");
+			b=scanner.next();
         if(a.equalsIgnoreCase(b)){
         	System.out.println("The two strings are the same.");
+        	run=true;
+        	break;
         }
         else{
         	System.out.println("The two strings are not the same.");
+        	
+     
         }
+        }while(run=true);
 	}
 
 }
